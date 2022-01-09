7a0e74ea1611f583dfdc2b7c37181e08a8878311
Dan Burzo <dan@danburzo.ro>
2022-01-10 00:01:16 +0200

    Bug fixes and improvements (see description)
    Library:
    
    * [wheel] invoke startGesture() *and* doGesture() on the first `wheel` event, so that its deltaY is not dropped.
    * [wheel] make zooming in & zooming out steps symmetrical
    * [wheel] when pressing/releasing Ctrl key repeatedly as part of a scroll, make sure we don't reset the scale and/or translation, respectively
    
    Demos:
    
    * [demo-buttons] Make the factor for Zoom In / Zoom Out buttons symmetrical
    * [demo-buttons] Limit the element's scale to 25%–400%
    * [demo-buttons] Show the element's current scale in the bottom right corner of the screen

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function normalizeWheel(e) {
 function midpoint(touches) {
 	let [t1, t2] = touches;
 	return {
-		x: (t1.clientX + t2.clientX) / 2, 
+		x: (t1.clientX + t2.clientX) / 2,
 		y: (t1.clientY + t2.clientY) / 2
 	};
 }
@@ -41,7 +41,7 @@ function angle(touches) {
 	let [t1, t2] = touches;
 	let dx = t2.clientX - t1.clientX;
 	let dy = t2.clientY - t1.clientY;
-	return Math.atan2(dy, dx) * 180 / Math.PI;
+	return (Math.atan2(dy, dx) * 180) / Math.PI;
 }
 
 function okzoomer(container, opts) {
@@ -57,51 +57,64 @@ function okzoomer(container, opts) {
 
 	let wheel_gesture = null;
 	let timer;
-	container.addEventListener('wheel', function wheelListener(e) {
-		if (e.cancelable !== false) {
-			e.preventDefault();
-		}
-		let [dx, dy] = normalizeWheel(e);
-		if (!wheel_gesture) {
-			wheel_gesture = {
-				origin: { x: e.clientX, y: e.clientY },
-				scale: 1,
-				translation: { x: 0, y: 0 }
-			};
-			startGesture(wheel_gesture);
-		} else {
-			if (e.ctrlKey) {
-				// pinch-zoom
-				wheel_gesture = {
-					origin: { x: e.clientX, y: e.clientY },
-					scale: wheel_gesture.scale * (1 - (WHEEL_SCALE_SPEEDUP * dy) / 100),
-					translation: { x: 0, y: 0 }
-				};
-			} else {
-				// panning
+	container.addEventListener(
+		'wheel',
+		function wheelListener(e) {
+			if (e.cancelable !== false) {
+				e.preventDefault();
+			}
+			let [dx, dy] = normalizeWheel(e);
+
+			if (!wheel_gesture) {
 				wheel_gesture = {
 					origin: { x: e.clientX, y: e.clientY },
 					scale: 1,
-					translation: { 
-						x: wheel_gesture.translation.x - WHEEL_TRANSLATION_SPEEDUP * dx, 
-						y: wheel_gesture.translation.y - WHEEL_TRANSLATION_SPEEDUP * dy
+					translation: { x: 0, y: 0 }
+				};
+				startGesture(wheel_gesture);
+			}
+
+			if (e.ctrlKey) {
+				// pinch-zoom gesture
+				let factor = dy <= 0 ? 
+					1 - WHEEL_SCALE_SPEEDUP * dy / 100 : 
+					1 / (1 + WHEEL_SCALE_SPEEDUP * dy / 100);
+				wheel_gesture = {
+					origin: { x: e.clientX, y: e.clientY },
+					scale: wheel_gesture.scale * factor,
+					translation: wheel_gesture.translation
+				};
+			} else {
+				// pan gesture
+				wheel_gesture = {
+					origin: { x: e.clientX, y: e.clientY },
+					scale: wheel_gesture.scale,
+					translation: {
+						x:
+							wheel_gesture.translation.x -
+							WHEEL_TRANSLATION_SPEEDUP * dx,
+						y:
+							wheel_gesture.translation.y -
+							WHEEL_TRANSLATION_SPEEDUP * dy
 					}
 				};
 			}
 			doGesture(wheel_gesture);
-		}
-		if (timer) {
-			window.clearTimeout(timer);
-		}
-		timer = window.setTimeout(function() {
-			if (wheel_gesture) {
-				endGesture(wheel_gesture);
-				wheel_gesture = null;
+
+			if (timer) {
+				window.clearTimeout(timer);
 			}
-		}, 200);
-	}, {
-		passive: false
-	});
+			timer = window.setTimeout(function () {
+				if (wheel_gesture) {
+					endGesture(wheel_gesture);
+					wheel_gesture = null;
+				}
+			}, 200);
+		},
+		{
+			passive: false
+		}
+	);
 
 	let initial_touches;
 	let touch_gesture = null;
@@ -110,10 +123,16 @@ function okzoomer(container, opts) {
 			let mp_init = midpoint(initial_touches);
 			let mp_curr = midpoint(e.touches);
 			touch_gesture = {
-				scale: e.scale !== undefined ? e.scale : distance(e.touches) / distance(initial_touches),
-				rotation: e.rotation !== undefined ? e.rotation : angle(e.touches) - angle(initial_touches),
-				translation: { 
-					x: mp_curr.x - mp_init.x, 
+				scale:
+					e.scale !== undefined
+						? e.scale
+						: distance(e.touches) / distance(initial_touches),
+				rotation:
+					e.rotation !== undefined
+						? e.rotation
+						: angle(e.touches) - angle(initial_touches),
+				translation: {
+					x: mp_curr.x - mp_init.x,
 					y: mp_curr.y - mp_init.y
 				},
 				origin: mp_init
@@ -125,63 +144,77 @@ function okzoomer(container, opts) {
 		}
 	}
 
-	container.addEventListener('touchstart', function watchTouches(e) {
-		if (e.touches.length === 2) {
-			initial_touches = e.touches;
-			touch_gesture = {
-				scale: 1,
-				rotation: 0,
-				translation: { x: 0, y: 0 },
-				origin: midpoint(initial_touches)
-			};
-			if (e.type === 'touchstart' && e.cancelable !== false) {
-				e.preventDefault();
+	container.addEventListener(
+		'touchstart',
+		function watchTouches(e) {
+			if (e.touches.length === 2) {
+				initial_touches = e.touches;
+				touch_gesture = {
+					scale: 1,
+					rotation: 0,
+					translation: { x: 0, y: 0 },
+					origin: midpoint(initial_touches)
+				};
+				if (e.type === 'touchstart' && e.cancelable !== false) {
+					e.preventDefault();
+				}
+				startGesture(touch_gesture);
+				container.addEventListener('touchmove', touchMove, {
+					passive: false
+				});
+				container.addEventListener('touchend', watchTouches);
+				container.addEventListener('touchcancel', watchTouches);
+			} else if (touch_gesture) {
+				endGesture(touch_gesture);
+				touch_gesture = null;
+				container.removeEventListener('touchmove', touchMove);
+				container.removeEventListener('touchend', watchTouches);
+				container.removeEventListener('touchcancel', watchTouches);
 			}
-			startGesture(touch_gesture);
-			container.addEventListener('touchmove', touchMove, { passive: false });
-			container.addEventListener('touchend', watchTouches);
-			container.addEventListener('touchcancel', watchTouches);
-		} else if (touch_gesture) {
-			endGesture(touch_gesture);
-			touch_gesture = null;
-			container.removeEventListener('touchmove', touchMove);
-			container.removeEventListener('touchend', watchTouches);
-			container.removeEventListener('touchcancel', watchTouches);
-		}
-	}, { passive: false });
+		},
+		{ passive: false }
+	);
 
 	if (
 		typeof GestureEvent !== 'undefined' &&
 		typeof TouchEvent === 'undefined'
 	) {
 		let in_gesture = false;
-		container.addEventListener('gesturestart', function handleGestureStart(e) {
-			if (!in_gesture) {
-				startGesture({
-					translation: { x: 0, y: 0 },
-					scale: e.scale,
-					rotation: e.rotation,
-					origin: { x: e.clientX, y: e.clientY }
-				});
-				in_gesture = true;
-			}
-			if (e.cancelable !== false) {
-				e.preventDefault();
-			}
-		}, { passive: false });
-		container.addEventListener('gesturechange', function handleGestureChange(e) {
-			if (in_gesture) {
-				doGesture({
-					translation: { x: 0, y: 0 },
-					scale: e.scale,
-					rotation: e.rotation,
-					origin: { x: e.clientX, y: e.clientY }
-				});
-			}
-			if (e.cancelable !== false) {
-				e.preventDefault();
-			}
-		}, { passive: false });
+		container.addEventListener(
+			'gesturestart',
+			function handleGestureStart(e) {
+				if (!in_gesture) {
+					startGesture({
+						translation: { x: 0, y: 0 },
+						scale: e.scale,
+						rotation: e.rotation,
+						origin: { x: e.clientX, y: e.clientY }
+					});
+					in_gesture = true;
+				}
+				if (e.cancelable !== false) {
+					e.preventDefault();
+				}
+			},
+			{ passive: false }
+		);
+		container.addEventListener(
+			'gesturechange',
+			function handleGestureChange(e) {
+				if (in_gesture) {
+					doGesture({
+						translation: { x: 0, y: 0 },
+						scale: e.scale,
+						rotation: e.rotation,
+						origin: { x: e.clientX, y: e.clientY }
+					});
+				}
+				if (e.cancelable !== false) {
+					e.preventDefault();
+				}
+			},
+			{ passive: false }
+		);
 		container.addEventListener('gestureend', function handleGestureEnd(e) {
 			if (in_gesture) {
 				endGesture({
@@ -211,21 +244,21 @@ function getOrigin(el, gesture) {
 		return {
 			x: gesture.origin.x - rect.x,
 			y: gesture.origin.y - rect.y
-		}
+		};
 	}
 	if (el instanceof SVGElement) {
 		let matrix = el.ownerSVGElement.getScreenCTM().inverse();
-		let pt = new DOMPoint(gesture.origin.x, gesture.origin.y); 
+		let pt = new DOMPoint(gesture.origin.x, gesture.origin.y);
 		return pt.matrixTransform(matrix);
 	}
 	throw new Error('Expected HTML or SVG element');
-};
+}
 
 function applyMatrix(el, matrix) {
 	if (el instanceof HTMLElement) {
 		el.style.transform = matrix;
 	} else if (el instanceof SVGElement) {
-		let transformList = el.transform.baseVal; 
+		let transformList = el.transform.baseVal;
 		let transform = transformList.createSVGTransformFromMatrix(matrix);
 		transformList.initialize(transform);
 	} else {
@@ -233,9 +266,4 @@ function applyMatrix(el, matrix) {
 	}
 }
 
-export { 
-	okzoomer, 
-	gestureToMatrix, 
-	getOrigin,
-	applyMatrix
-};
+export { okzoomer, gestureToMatrix, getOrigin, applyMatrix };
